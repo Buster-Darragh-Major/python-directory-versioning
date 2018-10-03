97e8e1d3305d76af2b9abdd9dcf1938c75aac734
Buster Darragh-Major <busterjdarraghmajor@gmail.com>
2018-10-04 11:53:34 +1300

    Complete listversions

--- listversions.py
+++ listversions.py
@@ -8,6 +8,13 @@ import re
 
 if __name__ == '__main__':
     root = os.path.join(os.getcwd(), '.versiondir')
+
+    version_count = 0
     for i in os.listdir(root):
-        m = re.match("\\S*\\[\\d*\\](\\.\\S*)?$", sys.argv[1])
-        
+        filename, ext = os.path.splitext(sys.argv[1])
+        m = re.match('.' + filename + "\[(\d*)\]" + ext + "$", i)
+        if m:
+            version_count += 1
+
+    for i in xrange(version_count):
+        print sys.argv[1] + '.' + str(i + 1)
